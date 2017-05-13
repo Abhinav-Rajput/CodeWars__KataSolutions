e9dffd8987c8424cd5c3865a337e67ba637a4016
ABHINAV Kumar Singh <ABHINAV Kumar Singh>
2017-05-13 16:06:47 +0530

    build Tower

---
+++ Python Solutions/build_Towers.py
@@ -0,0 +1,13 @@
+def tower_builder(n_floors):
+    if n_floors == 0:
+        return []
+        
+    count = 1
+    result = []
+
+    for i in range(1, n_floors + 1):
+      stars = '*' * (2 * i - 1)
+      space = ' ' * (n_floors - i)
+      result.append(space + stars + space)            
+    return result
+    
